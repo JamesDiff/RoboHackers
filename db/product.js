fb4e101936c052219df88544a8d72be9011b8a4b
spencerporter <scfp419@gmail.com>
2021-12-02 21:08:36 -0700

    Added the getProductById for single product view

--- db/product.js
+++ db/product.js
@@ -33,7 +33,23 @@ async function getAllProducts() {
     }
 }
 
+async function getProductById(id){
+    try {
+        const {rows: [product]} = await client.query(`
+            SELECT *
+            FROM products
+            WHERE id=$1;
+        `, [id]);
+
+        return product;
+    }catch (error){
+        console.error("Error getting product with ID ", id)
+        throw error;
+    }
+}
+
 module.exports = {
     createProduct,
-    getAllProducts
+    getAllProducts,
+    getProductById
 }
