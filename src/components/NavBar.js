74f40f5eb715974f66396b594efd4304b42ad08a
clairepender <clairepender5@gmail.com>
2021-12-04 11:37:00 -0600

    test

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+
+    return (
+        <nav>
+            {(!token ? 
+                    <div id="nav-bar-no-token">
+                    <Link to="/">Home | </Link>
+                    <Link to="/register">Sign Up | </Link>
+                    <Link className="nav-link active" to="/login">Login </Link> 
+                    </div>
+                    : 
+                    
+                    <div id="nav-bar-with-token">
+                    <Link to="/">Home | </Link>
+                    <Link className="nav-link active" to="#" onClick={(event) => {
+                                localStorage.removeItem("token");
+                                setToken("");
+                        }}>Log Out</Link>
+
+                    </div>)}
+        </nav>
+
+    )
+}
+
+export default NavBar;
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as App } from './App';
+export { default as SingleProductView } from './SingleProductView'
