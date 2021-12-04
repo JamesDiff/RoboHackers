fe6fd66a88d45dd43c63bcf664d4ae38772e5349
Your Name <your_email@real_email.edu>
2021-12-04 12:34:24 -0700

    debugged

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const apiRouter = require('express').Router();
 
-apiRouter.get("/", (req, res, next) => {
-  res.send({
-    message: "API is under construction!"
-  });
-});
+// // apiRouter.get("/", (req, res, next) => {
+//   res.send({
+//     message: "API is under construction!"
+//   });
+//   next()
+// });
 
-module.exports = apiRouter;
 
 const productsRouter = require('./products');
-apiRouter.use('./products', productsRouter);
+apiRouter.use('/products', productsRouter);
+
+
+module.exports = apiRouter;
