d3eb67a5c1a0e18d276c001a1287f6509013c569
Derek Miller <derekmiller@Dereks-MacBook-Pro.local>
2021-12-04 09:37:35 -0700

    worked on FE Login, Logout, Register, and their respective api calls

--- src/api/index.js
+++ src/api/index.js
@@ -9,10 +9,93 @@ import axios from 'axios';
 //   }
 // }
 
+export const registerUser = async (setToken, username, password, verifyPassword, email, firstname, lastname, city, state, zipcode, phone) => {
+
+  try {
+    if (password !== verifyPassword) {
+      alert("Passwords DO NOT match!!! 🤦‍♂️");
+      return;
+    }
+
+    const response = await fetch('/api/users/register', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application.json'
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        email: email,
+        firstname: firstname,
+        lastname: lastname,
+        city: city,
+        state: state,
+        zipcode: zipcode,
+        phone: phone
+      })
+    })
+    const result = await response.json();
+    console.log(result);
+    const user = result.user;
+    const token = result.token;
+    console.log("New registered user is: ", user);
+    setToken(token);
+    localStorage.setItem("token", token);
+    if (result.error) throw result.error;
+    
+  } 
+  
+  catch (error) {
+    console.error("ERROR registering new user!!! 🤦‍♂️");
+    throw error;
+  }
+}
+
+export const loginUser = async (username, password, setToken) => {
+
+  try {
+    const response = await fetch('/api/users/login', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application.json'
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+
+    const result = await response.json();
+    console.log(result);
+    console.log(result.token);
+    const token = result.token;
+    setToken(token);
+    localStorage.setItem("token", token);
+    localStorage.getItem("token");
+    if (result.error) throw result.error;
+    
+  } 
+  
+  catch (error) {
+    console.error("ERROR logging in user!!! 🤦‍♂️");
+    throw error;
+  }
+}
+
 export const getAllProducts = () => {
   try {
-    const { data } = await axios.get('/api/products');
-    return data;
+    const response = await fetch('/api/products', {
+      headers: {
+        'Content-Type': 'application.json'
+      },
+    })
+
+    const result = await response.json();
+
+    if (result.error) throw result.error;
+    console.log(result);
+    return result;
+    
   } 
   
   catch (error) {
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
 import AllProducts from './allproducts';
+import { Login, Logout } from './login';
+import Register from './register';
 
 export { default as App } from './App';
-export { AllProducts, }
+export { AllProducts,
+        Login,
+        Logout,
+        Register, }
