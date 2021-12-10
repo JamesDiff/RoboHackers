011e54b41c27c564ee9f7c06839a342f104ca31d
spencerporter <scfp419@gmail.com>
2021-12-09 20:39:00 -0700

    Add ability to add and retrieve line items on an order as well as getting orders that belong to a email(aka username)

---
+++ db/order_products.js
@@ -0,0 +1,37 @@
+const client = require('./client');
+
+async function addProductToOrder(order, product, quantity){
+    console.log("Adding Product to Order", product.price, order, quantity)
+    try{
+        const {rows : [productToOrder]} = await client.query(`
+            INSERT INTO order_products("orderId", "productId", priceAtTimeOfOrder, quantity)
+            VALUES ($1, $2, $3, $4)
+            ON CONFLICT ("orderId", "productId") DO NOTHING
+            RETURNING *;
+        `, [order.id, product.id, product.price, quantity]);
+        return productToOrder;
+    }catch(error){
+        console.error("Error adding Product to Order", error);
+        throw error;
+    }
+}
+
+async function getOrderProductsByOrder(orderId){
+    try{
+        const {rows : lineItems} = await client.query(`
+            SELECT products.*, order_products.priceAtTimeOfOrder, order_products.quantity
+            FROM products
+            JOIN order_products ON products.id=order_products."productId"
+            WHERE order_products."orderId"=$1;
+        `, [orderId]);
+        return lineItems;
+    }catch(error){
+        console.error("Error getting Orders Line Items", error);
+        throw error;
+    }
+}
+
+module.exports = {
+    addProductToOrder,
+    getOrderProductsByOrder
+}
