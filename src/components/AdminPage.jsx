ca541cedafe72fb12bf79a9d3c020bdfb51360a1
Derek Miller <da970miller@gmail.com>
2021-12-22 22:00:04 -0700

    Some styling on admin page

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -7,15 +7,33 @@ const AdminPage = () => {
 
     return (<div id="product-box" className="form-group centered w-75">
                 <br />
-                <h1>
-                    <b style={{ color: 'darkorange' }}>*** ADMIN PAGE ***</b>
-                </h1>
+                <div className="d-flex justify-content-between p-3">
+                    <img src="https://static2.bigstockphoto.com/6/9/2/large2/296101924.jpg" alt="orange admin"></img>
+                    <img src="https://previews.123rf.com/images/carmendorin/carmendorin1405/carmendorin140500034/28009966-grunge-rubber-stamp-with-text-admin-vector-illustration.jpg" alt="admin"></img>
+                    <img src="https://cdn.w600.comps.canstockphoto.com/administration-and-computer-mouse-picture_csp10524671.jpg" alt="admin mouse"></img>
+                    <img src="https://static2.bigstockphoto.com/6/9/2/large2/296101924.jpg" alt="orange admin"></img>
+                    <img src="https://cdn.w600.comps.canstockphoto.com/administration-and-computer-mouse-picture_csp10524671.jpg" alt="admin mouse"></img>
+                    <img src="https://previews.123rf.com/images/carmendorin/carmendorin1405/carmendorin140500034/28009966-grunge-rubber-stamp-with-text-admin-vector-illustration.jpg" alt="admin"></img>
+                    <img src="https://static2.bigstockphoto.com/6/9/2/large2/296101924.jpg" alt="orange admin"></img>
+                </div>
                 <br />
-                <Link to="/admin-create" className="btn btn-success btn-lg btn-block centered w-50">CREATE NEW PRODUCT</Link>
+                <Link to="/admin-create" className="btn btn-danger btn-lg btn-block centered w-50">CREATE NEW PRODUCT</Link>
                 <br />
-                <Link to="/admin-products" className="btn btn-primary btn-info btn-lg btn-block centered w-50">ALL PRODUCTS</Link>
+                <Link to="/admin-products" className="btn btn-danger btn-info btn-lg btn-block centered w-50">ALL PRODUCTS</Link>
                 <br />
-                <Link to="/admin-users" className="btn btn-primary btn-warning btn-lg btn-block centered w-50">ALL USERS</Link>
+                <Link to="/admin-users" className="btn btn-primary btn-danger btn-lg btn-block centered w-50">ALL USERS</Link>
+                <br />
+                <Link to="/admin-orders" className="btn btn-primary btn-danger btn-lg btn-block centered w-50">ALL ORDERS</Link>
+                <br />
+                <div className="d-flex justify-content-between p-3">
+                    <img src="https://static2.bigstockphoto.com/6/9/2/large2/296101924.jpg" alt="orange admin"></img>
+                    <img src="https://previews.123rf.com/images/carmendorin/carmendorin1405/carmendorin140500034/28009966-grunge-rubber-stamp-with-text-admin-vector-illustration.jpg" alt="admin"></img>
+                    <img src="https://cdn.w600.comps.canstockphoto.com/administration-and-computer-mouse-picture_csp10524671.jpg" alt="admin mouse"></img>
+                    <img src="https://static2.bigstockphoto.com/6/9/2/large2/296101924.jpg" alt="orange admin"></img>
+                    <img src="https://cdn.w600.comps.canstockphoto.com/administration-and-computer-mouse-picture_csp10524671.jpg" alt="admin mouse"></img>
+                    <img src="https://previews.123rf.com/images/carmendorin/carmendorin1405/carmendorin140500034/28009966-grunge-rubber-stamp-with-text-admin-vector-illustration.jpg" alt="admin"></img>
+                    <img src="https://static2.bigstockphoto.com/6/9/2/large2/296101924.jpg" alt="orange admin"></img>
+                </div>
             </div>)
 
 }
