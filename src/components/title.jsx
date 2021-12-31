50c401b3be47e861a326c5c59f53b8ebbfbb94fc
Derek Miller <da970miller@gmail.com>
2021-12-30 20:54:43 -0700

    Styled the Title Component

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 // This is just my title header with the title of my project and my name as the developer.
 
 const Title = () => {
-    return  <div id="title" className="jumbotron text-center">
+    return  <div id="title container" className="jumbotron text-center">
                 
-                <h1 className="text-info"><b>GraceShopper</b></h1>
-                <h5 className="text-warning"><b>Developed by Team ROBOHACKERS</b></h5>
+                <h1 className="text-primary font-anton"><b>GraceShopper</b></h1>
+                <h5 className="text font-vuj"><b>Developed by Team</b></h5>
+                <h1 className='text-danger font-amatic'><b>ROBOHACKERS</b></h1>
                 
                 
             </div>
