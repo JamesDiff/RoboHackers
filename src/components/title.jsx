75f5c64a64f8784430d98c114b18989e5237dee2
Derek Miller <da970miller@gmail.com>
2021-12-30 21:30:22 -0700

    Added a shadow

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 // This is just my title header with the title of my project and my name as the developer.
 
 const Title = () => {
-    return  <div id="title container" className="jumbotron text-center">
+    return  <div id="title card centered" className="jumbotron centered shadow-lg">
                 
                 <h1 className="text-primary font-anton"><b>GraceShopper</b></h1>
                 <h5 className="text font-vuj"><b>Developed by Team</b></h5>
-                <h1 className='text-danger font-amatic'><b>ROBOHACKERS</b></h1>
-                
-                
+                <h1 className='text-danger font-amatic shadow-lg w-25 centered'><b>ROBOHACKERS</b></h1>
             </div>
 
 }
