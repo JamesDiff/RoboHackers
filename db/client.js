3469e6f0ee90193b88c0d74c99095df7a41b49e3
Your Name <your_email@real_email.edu>
2022-04-14 06:57:42 -0600

    updated database connection for heroku

--- db/client.js
+++ db/client.js
@@ -1,6 +1,14 @@
 // Connect to DB
 const { Client } = require('pg');
 const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ process.env.DB_NAME }`;
-const client = new Client(DB_URL);
+// const client = new Client(DB_URL) for local host
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+            
+        }
+});
 
 module.exports = client;
