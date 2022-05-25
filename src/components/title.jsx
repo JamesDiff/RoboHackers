cf0d7c3af6faf571d4ddd3ec20c4f6e9792889df
Your Name <your_email@real_email.edu>
2022-05-25 12:24:04 -0600

    Just tweeking some things

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 const Title = () => {
     return  <div id="title card centered" className="jumbotron centered shadow-lg">
                 
-                <Link to='/'><h1 className="site-title"><b>DVDungeon</b></h1></Link>
+                <Link to='/'><h1 className="site-title"><b>DVDUNGEON</b></h1></Link>
              
             </div>
 
 }
+
+//set up database build command on heroku; want to be able to run db command on heroku-- figure out the correct syntax
 
 export default Title;
